fae387204628b73a65b12e9b061c27edfb6a016b
Giorgi Botchorishvili <herr.giorgi@gmail.com>
2017-09-23 23:19:45 +0200

    sent coupon info to provider

--- broadleaf-5.0.1/admin/src/main/java/com/paycom/controller/CouponController.java
+++ broadleaf-5.0.1/admin/src/main/java/com/paycom/controller/CouponController.java
@@ -1,5 +1,7 @@
 package com.paycom.controller;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,10 +11,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.broadleafcommerce.common.email.domain.EmailTargetImpl;
+import org.broadleafcommerce.common.email.service.EmailService;
+import org.broadleafcommerce.common.email.service.info.EmailInfo;
 import org.broadleafcommerce.common.exception.ServiceException;
-import org.broadleafcommerce.core.catalog.service.CatalogService;
 import org.broadleafcommerce.openadmin.dto.ClassMetadata;
 import org.broadleafcommerce.openadmin.dto.Entity;
 import org.broadleafcommerce.openadmin.dto.Property;
@@ -20,6 +25,8 @@ import org.broadleafcommerce.openadmin.dto.SectionCrumb;
 import org.broadleafcommerce.openadmin.web.controller.entity.AdminBasicEntityController;
 import org.broadleafcommerce.openadmin.web.controller.modal.ModalHeaderType;
 import org.broadleafcommerce.openadmin.web.form.entity.EntityForm;
+import org.broadleafcommerce.profile.core.domain.Customer;
+import org.broadleafcommerce.profile.core.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
@@ -31,7 +38,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bali.core.order.service.CouponService;
-import com.bali.core.promo.Coupon;
 import com.google.common.collect.Lists;
 
 @Controller
@@ -41,7 +47,10 @@ public class CouponController extends AdminBasicEntityController {
 	private static final Log logger = LogFactory.getLog(CouponController.class);
 
 	protected static final String SECTION_KEY = "coupons";
-	// @Resource(name = "couponService")
+	@Resource(name = "blCustomerService")
+	private CustomerService customerService;
+	@Resource(name = "blEmailService")
+	private EmailService emailService;
 	@Autowired
 	private CouponService couponService;
 
@@ -89,14 +98,82 @@ public class CouponController extends AdminBasicEntityController {
 		if (CollectionUtils.isEmpty(properties)) {
 			return;
 		}
+		List<Entity> newEntities = Lists.newArrayList(entity);
 		Long count = Long.valueOf(properties.get(0).getValue());
-		if(count == null) {
+		if (count == null) {
 			return;
 		}
 		for (int i = 1; i < count; i++) {
 			logger.info("generate coupons for :" + entity.getType()[0]);
-			service.addEntity(entityForm, sectionCriteria, sectionCrumbs).getEntity();
+			newEntities.add(service.addEntity(entityForm, sectionCriteria, sectionCrumbs).getEntity());
 		}
+		sendGeneratedCouponInfoToProvider(newEntities);
+	}
+
+	private void sendGeneratedCouponInfoToProvider(List<Entity> newEntities) {
+		if (CollectionUtils.isEmpty(newEntities)) {
+			return;
+		}
+		// read provider info
+		String providerId = "";
+		String couponName = "";
+		String validFrom = "";
+		String validTo = "";
+		for (Property property : newEntities.get(0).getProperties()) {
+			switch (property.getName()) {
+			case "provider":
+				providerId = property.getValue();
+				break;
+			case "name":
+				couponName = property.getValue();
+				break;
+			case "validFrom":
+				validFrom = property.getValue();
+				break;
+			case "validTo":
+				validTo = property.getValue();
+				break;
+			default:
+				break;
+			}
+		}
+		if(StringUtils.isBlank(providerId)) {
+			return;
+		}
+		// read coupon codes
+		List<String> couponCodes = newEntities.stream().map(e -> e.getProperties())
+				.flatMap(p -> Arrays.asList(p).stream()).filter(p -> "code".equals(p.getName())).map(p -> p.getValue())
+				.collect(Collectors.toList());
+		
+		HashMap<String, Object> vars = new HashMap<String, Object>();
+		Customer provider = customerService.readCustomerById(Long.valueOf(providerId));
+		vars.put("name", provider.getFirstName() + " " + provider.getLastName());
+		vars.put("comments", "");
+		vars.put("emailAddress", provider.getEmailAddress());
+		
+		EmailInfo emailInfo = new EmailInfo();
+		
+		emailInfo.setFromAddress("info@paycom.ge");
+		emailInfo.setSubject("Paycom generated coupons ");
+		StringBuffer messageBodyBuilder = new StringBuffer();
+		messageBodyBuilder.append("Generated coupons: " + newEntities.size());
+		messageBodyBuilder.append("<br />");
+		messageBodyBuilder.append("Coupon name: " + couponName);
+		messageBodyBuilder.append("<br />");
+		messageBodyBuilder.append("valid from: " + validFrom + " until: " + validTo);
+		messageBodyBuilder.append("<br />");
+		messageBodyBuilder.append("Codes:");
+		messageBodyBuilder.append(String.join("<br />", couponCodes));
+		emailInfo.setMessageBody(messageBodyBuilder.toString());
+		EmailTargetImpl emailTarget = new EmailTargetImpl();
+		
+		emailTarget.setEmailAddress(provider.getEmailAddress());
+		try {
+			emailService.sendBasicEmail(emailInfo, emailTarget, vars);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
